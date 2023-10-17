7b15bfde27a8ef6c9a9d6659c80a20f058bff318
Mutesa-Cedric <mutesacedric@gmail.com>
2023-10-17 10:57:22 +0200

    init : setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="bg-gray-200 w-full h-screen flex items-center justify-center">
+      <p className="text-blue-500 font-semibold text-xl">Hello there!</p>
+    </div>
+  )
+}
+
+export default App
